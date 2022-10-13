5197acb03fef90d0bd4e7ea2c75f472dfd08c780
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-10-13 10:49:43 +0530

    [Shivaraj] Refactor: doc string of the function is changed.

--- UserRegistration/Regex-UserRegistration.py
+++ UserRegistration/Regex-UserRegistration.py
@@ -2,7 +2,7 @@
     @Author: Shivaraj
     @Date: 10-10-2022
     @Last Modified by: Shivaraj
-    @Last Modified date: 11-10-2022
+    @Last Modified date: 13-10-2022
     @Title: As a User need to enter a valid Last Name
             - Last name starts with Cap and has minimum 3 characters
 '''
@@ -17,10 +17,6 @@ lg = get_logger(name="(Validate last Name)", file_name="data_log.log")
 class UserRegistration:
 
     def __init__(self):
-        """
-        Container for regex pattern and valdating user input with this patterns.
-        """
-
         self.regex_name = '^[A-Z][a-z]{2,}$'
 
         # last_name:            A          bc
@@ -29,11 +25,11 @@ class UserRegistration:
     def get_first_name(self, first_name):
         """
         Description:
-            Takes the parameter None but return the validation of first name after matching regex pattern.
+            This function is used to check whether the first name starts with Cap and has minimum 3 characters
         Parameter:
-            Passed parameter is None
+            first_name: The first_name to be checked
         Return:
-            Returns nothing but print the validation of first name after matching regex pattern..
+            None
         """
         try:
             matches = re.search(self.regex_name, first_name)
@@ -48,11 +44,11 @@ class UserRegistration:
     def get_last_name(self, last_name):
         """
         Description:
-            Takes the parameter None but return the validation of last name after matching regex pattern.
+            This function is used to check whether the last name starts with Cap and has minimum 3 characters
         Parameter:
-            Passed parameter is None
+            last_name: The last_name to be checked
         Return:
-            Returns nothing but print the validation of last name after matching regex pattern..
+            None
         """
         try:
             matches = re.search(self.regex_name, last_name)
