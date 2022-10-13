59dd682338f4aee7e42e52211c185c4e4ab24660
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-10-13 10:42:37 +0530

    [Shivaraj] Refactor: doc string of the function is changed.

--- UserRegistration/Regex-UserRegistration.py
+++ UserRegistration/Regex-UserRegistration.py
@@ -2,7 +2,7 @@
     @Author: Shivaraj
     @Date: 10-10-2022
     @Last Modified by: Shivaraj
-    @Last Modified date: 10-10-2022
+    @Last Modified date: 13-10-2022
     @Title: As a User need to enter a valid First Name
             - First name starts with Cap and has minimum 3 characters
 '''
@@ -17,9 +17,6 @@ lg = get_logger(name="(Validate First Name)", file_name="data_log.log")
 class UserRegistration:
 
     def __init__(self):
-        """
-        Container for regex pattern and valdating user input with this patterns.
-        """
         self.regex_name = '^[A-Z][a-z]{2,}$'
 
         # first_name:           A          bc
@@ -28,19 +25,18 @@ class UserRegistration:
     def get_first_name(self, first_name):
         """
         Description:
-            Takes the parameter None but return the validation of first name after matching regex pattern.
+            This function is used to check whether the first name starts with Cap and has minimum 3 characters
         Parameter:
-            Passed parameter is None
+            first_name: The first_name to be checked 
         Return:
-            Returns nothing but print the validation of first name after matching regex pattern..
+            None
         """
         try:
             matches = re.search(self.regex_name, first_name)
             if matches:
                 lg.info(f'First name validated successfully: {first_name}')
             else:
-                lg.info(
-                    "Please re-enter the first name with name starts with capital and has minimum 3 characters")
+                lg.info("Please re-enter the first name with name starts with capital and has minimum 3 characters")
         except Exception as e:
             lg.exception(e)
 
